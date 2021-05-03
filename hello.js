1bbe6e95081988759a24416e7d9b1ac0c9619e0a
DerekThompson80 <>
2021-05-03 19:58:10 +0000

    fifth try

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 console.log( "Hello world!"); {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello!";
+	return "Hello world!";
 }
  
-module.exports = hello;
+module.exports = Hello world!;
 console.log( "Hello world!");
