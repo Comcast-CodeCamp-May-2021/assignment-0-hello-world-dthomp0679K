685a198ee5b9725bd7344f833a67bb7f577e62aa
DerekThompson80 <>
2021-05-03 20:13:49 +0000

    try 9

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
-return "Hello world!"; {
+return Hello world!; {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
 	return "Hello world!";
 }
  
 module.exports = Hello world!;
-return "Hello world!";
+return Hello world!;
