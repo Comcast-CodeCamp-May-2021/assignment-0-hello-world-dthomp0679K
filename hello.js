f05b6e676275d1216c2528cf2c4311b9013edad9
DerekThompson80 <>
2021-05-03 20:05:40 +0000

    I am not sure what wrong here. try 6

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
-console.log( "Hello world!"); {
+console.log("Hello world!"); {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
 	return "Hello world!";
 }
  
 module.exports = Hello world!;
-console.log( "Hello world!");
+console.log("Hello world!");
